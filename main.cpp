be6b6a4106f1aa13f289f0ef55c3512f3e6b2e2f
Aman04s <649321@pdsb.net>
2022-05-25 10:52:44 -0400

    start

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <stdlib.h>
+#include <fstream>
+
+using namespace std; 
+
+void Enterance(){
+
+}
+
+int main(){
+    Enterance();
+    
+}

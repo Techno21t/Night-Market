3054de4d660b3a9d21cfa9aab8537c45f7e1849d
Zero <aman.tanvi@icloud.com>
2022-05-27 10:40:07 -0400

    idk

--- main.cpp
+++ main.cpp
@@ -3,10 +3,17 @@
 #include <string>
 #include <stdlib.h>
 #include <fstream>
+#include <iomanip>
 
 using namespace std; 
 
 void Enterance(){
+    cout<<setw(25)<<"*********************************"<<endl;
+    cout<<setw(25)<<"          Night Market           "<<endl;
+    cout<<setw(25)<<"*********************************"<<endl;    
+     time_t now = time(0);
+     char* dt = ctime(&now);
+     cout <<setw(25)<< dt << endl;
 
 }
 

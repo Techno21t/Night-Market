171a65cc496f46a7773820a1e04c81d7d538280c
Aman04s <649321@pdsb.net>
2022-05-30 09:26:50 -0400

    yaaa

--- main.cpp
+++ main.cpp
@@ -8,12 +8,13 @@
 using namespace std; 
 
 void Enterance(){
-    cout<<setw(25)<<"*********************************"<<endl;
-    cout<<setw(25)<<"          Night Market           "<<endl;
-    cout<<setw(25)<<"*********************************"<<endl;    
+    cout<<"*********************************"<<endl;
+    cout<<"          Night Market           "<<endl;
+    cout<<"*********************************"<<endl;    
      time_t now = time(0);
      char* dt = ctime(&now);
-     cout <<setw(25)<< dt << endl;
+     cout<< dt << endl;
+     system("test.cpp");
 
 }
 
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <stdlib.h>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    cout <<"sucka dik ";
+}

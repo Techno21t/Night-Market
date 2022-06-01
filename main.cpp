30494f4fc35aaaab1e52613d618458b78488eabd
Aman04s <649321@pdsb.net>
2022-06-01 11:34:50 -0400

    idfk

--- main.cpp
+++ main.cpp
@@ -4,21 +4,73 @@
 #include <stdlib.h>
 #include <fstream>
 #include <iomanip>
+#include <mysql.h>
 
 using namespace std; 
 
+//universal//
+void user();
+void Enterance();
+void Market();
+void Sell();
+void Auction();
+
+
+class Annonymous{
+    public:
+    string ID;
+    string response;
+};
+
+void user(){
+    Annonymous Info;
+    cout<<endl<<endl<<endl<<endl<<"                                   Name: ";
+    cin>>Info.ID;
+    system("CLS");
+    Enterance();
+}
+
 void Enterance(){
-    cout<<"*********************************"<<endl;
-    cout<<"          Night Market           "<<endl;
-    cout<<"*********************************"<<endl;    
+    Annonymous Info;
+    cout<<"                                          *********************************"<<endl;
+    cout<<"                                          |         Night Market          |"<<endl;
+    cout<<"                                          *********************************"<<endl;    
      time_t now = time(0);
      char* dt = ctime(&now);
-     cout<< dt << endl;
-     system("test.cpp");
+     cout<<"                                               "<< dt << endl;
+     cout<<"                                          1. Market Place"<<endl;
+     cout<<"                                          2. Selling station"<<endl;
+     cout<<"                                          3. Auction House (Work in Progress)"<<endl;
+     cout<<"                                                    ";
+     cin>>Info.response;
+     choice();
+}
+
+void choice(){
+    Annonymous Info;
+    if(Info.response == "1"){
+        Market();
+    }if(Info.response == "2"){
+        Sell();
+    }if(Info.response == "3"){
+        Auction();
+    }
+}
+//makret place menu//
+void Market(){
+
+}
+//selling station//
+void Sell(){
+
+}
+//Auction house//
+void Auction(){
+
+
 
 }
 
 int main(){
-    Enterance();
-    
+    user();
 }
